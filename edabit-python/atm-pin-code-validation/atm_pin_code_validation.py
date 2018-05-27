1e04e483f0b8230647f343e858f3a282579b3dc4
mikaelblomkvist50 <mikaelblomkvist50@gmail.com>
2018-05-27 20:14:21 +1000

    Add.

--- edabit-python/atm-pin-code-validation/atm_pin_code_validation.py
+++ edabit-python/atm-pin-code-validation/atm_pin_code_validation.py
@@ -4,7 +4,12 @@ def is_valid_PIN(pin):
     else:
         return False
 
-#Pseudo code
+# If the string includes special character we instanly know it's not valid.
+# However if we know its a valid string length, it does not necessarily mean it is a valid pin since
+# it could be a valid string length with a special character. Hence it's best to check for spcial characters first.
+# Then make sure the string has a valid length (4 or 6).
+
+# Pseudo code
 
 # if string contain ! special characters && string length is 4 || 6
 #     return True
